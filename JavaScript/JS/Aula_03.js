58c2ffddce98c5c2ae4e0ad2b0f4858ed81a0c11
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-26 01:55:18 -0300

    // Browser Object Model (BOM)
    Aula 03 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_03.js
@@ -0,0 +1,11 @@
+// Browser Object Model (BOM)
+
+// Window
+// frames   history location    navigator   screen
+
+// Document Object Model
+// document
+// anchors  applets areas   embeds
+// forms    image   layers  links
+
+console.log(screen.width)
