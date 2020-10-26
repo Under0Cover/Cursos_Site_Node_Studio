b309b21a748f404c2d68f06ddbd479360b7eeddf
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-26 09:33:28 -0300

    [DOM] querySelectorAll
    Aula 07 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_07.js
@@ -0,0 +1,3 @@
+console.log(document.querySelectorAll(".times"))
+console.log(document.getElementById("futebol-europeu").querySelectorAll(".times"))
+console.log(document.getElementById("futebol-europeu").querySelectorAll(".times")[0].innerHTML)
