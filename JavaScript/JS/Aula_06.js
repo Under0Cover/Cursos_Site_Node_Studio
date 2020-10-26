b9702efdd1a65f5a4145f4e5b0649151bf26492e
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-26 09:17:43 -0300

    [DOM] querySelectorAll
    Aula 06 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_06.js
@@ -0,0 +1,8 @@
+console.log(document.getElementsByTagName("div"))
+console.log(document.getElementsByTagName("div")[0])
+console.log(document.getElementsByTagName("div")[1].innerHTML)
+console.log(document.getElementsByTagName("br"))
+console.log(document.getElementsByTagName("p"))
+console.log(document.querySelector("div"))
+console.log(document.querySelector(".div_02"))
+console.log(document.querySelector("#paragrafo_02"))
