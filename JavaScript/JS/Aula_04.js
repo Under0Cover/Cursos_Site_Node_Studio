a6235efce0628d27cf731bec90241a39d68f1216
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-26 02:16:52 -0300

    // Browser Object Model (BOM)
    Aula 04 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_04.js
@@ -0,0 +1,11 @@
+// Browser Object Model (BOM)
+
+console.log(screen.width)
+console.log(screen.height)
+console.log(screen.orientation)
+console.log(screen.orientation.angle)
+console.log(screen.orientation.type)
+console.log(screen.orientation.onchange)
+console.log(navigator)
+console.log(location)
+console.log(history)
