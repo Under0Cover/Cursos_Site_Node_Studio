0cda9a75770c43036d17625d965f2167351e4a03
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-27 08:17:01 -0300

    [DOM] createElement
    Aula 09 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_09.js
@@ -0,0 +1,10 @@
+console.log(document.createElement("div"))
+console.log(document.createElement("p"))
+
+var imagem = document.createElement("img")
+imagem.src = "imagens/js.png"
+console.log(imagem)
+
+var paragrafo = document.createElement("p")
+paragrafo.innerHTML = "Olá, pessoal"
+console.log(paragrafo)
