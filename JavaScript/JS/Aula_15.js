60162e70c215242840e1bb1d3a10babbf59075a8
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-27 10:54:45 -0300

    [DOM] createTextNode e textContent
    Aula 15 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_15.js
@@ -0,0 +1,6 @@
+var titulo = document.querySelector("h1")
+var titulo_2 = document.querySelector("h2")
+var texto = document.createTextNode("Texto criado por Create Text Node")
+
+titulo.appendChild(texto)
+titulo_2.textContent = "Texto criado por Text Content"
