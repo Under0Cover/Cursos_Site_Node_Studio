381d468241badf7e9a12b998ec43ecaf1cff10b0
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-28 18:35:42 -0300

    Arrays em javascript
    Aula 20 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_20.js
@@ -0,0 +1,22 @@
+// Array
+// Estrutura de dados que armazena uma coleção de elementos
+
+let array = []
+console.log(array)
+
+let meu_array = ["Notebook", 1500.00, {Memoria: '16Gb', HD: '100Gb'}, ["Notebook", "Mouse", "Teclado", "Tela"]]
+// Array = []
+// Objeto = {}
+
+console.log(meu_array)
+console.log(meu_array.length)
+console.log(meu_array[0])
+console.log(meu_array[1])
+console.log(meu_array[2])
+console.log(meu_array[2].Memoria)
+console.log(meu_array[2].HD)
+console.log(meu_array[3])
+console.log(meu_array[3][0])
+console.log(meu_array[3][1])
+console.log(meu_array[3][2])
+console.log(meu_array[3][3])
