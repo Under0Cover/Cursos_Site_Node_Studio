0299b3df7a83da94ed63804e24526e0d4675dd9b
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-29 09:36:32 -0300

    Operadores Aritméticos
    Aula 22 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_22.js
@@ -0,0 +1,22 @@
+// Operadores Aritméticos
+
+
+let x = 10
+let y = 5
+let subtracao = x - y
+console.log(subtracao)
+let adicao = x + y
+console.log(adicao)
+let multiplicacao = x * y
+console.log(multiplicacao)
+let divisao = x / y
+console.log(divisao)
+let exponenciacao = x ** y
+console.log(exponenciacao)
+let resto_divisao = x % y
+console.log(resto_divisao)
+
+// Média
+let z = 8
+let media = ((x + y + z) / 3 )
+console.log(media)
