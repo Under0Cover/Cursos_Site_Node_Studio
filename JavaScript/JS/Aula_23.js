2f1a5f284015792f0ba123b7faa7b8f226fb1edb
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-29 10:09:31 -0300

    Operadores de Atribuição
    Aula 23 - Curso de JavaScript

---
+++ JavaScript/JS/Aula_23.js
@@ -0,0 +1,30 @@
+// Operadores Aritméticos
+
+let x = 5
+let y = 2
+
+// x = x + y
+x += y
+console.log(x)
+
+x -= y
+console.log(x)
+
+x *= y
+console.log(x)
+
+x /= y
+console.log(x)
+
+x **= y
+console.log(x)
+
+x %= y
+console.log(x)
+
+x = 10
+x++
+console.log(x)
+
+x--
+console.log(x)
